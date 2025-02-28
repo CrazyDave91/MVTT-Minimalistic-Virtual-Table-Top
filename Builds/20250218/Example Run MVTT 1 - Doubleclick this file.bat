789369e2c38e44a9eb702ac92a91bc46d3494e92
Stefan PC <stefanslooten1991@gmail.com>
2025-02-28 18:59:28 +0100

    added v3 builds

--- Builds/20250218/Example Run MVTT 1 - Doubleclick this file.bat
+++
@@ -1,6 +0,0 @@
-: This is an example script to execute MVTT with preset parameters as described
-: Nr    Parameter         Description
-: -     Path to Script    Can be in the same folder as the batch script. Just update the relative path
-: 0     Path to Image     Can be given relative to the .exe file
-: 1     Monitor Number    Selects the monitor indox for the second window. Valid values: 0...[Monitors connected minus 1]
-start MVTT.exe default.jpg 1
--- Builds/20250221/Example Run MVTT 2 - Drag image onto this file.bat
+++
@@ -1,7 +0,0 @@
-: This is an example script to execute MVTT with a preselected monitor. The image can just be dragged onto the batch file
-: Nr    Parameter         Description
-: -     Path to Script    Can be in the same folder as the batch script. Just update the relative path
-: 0     Path to Image     Can be given relative to the .exe file
-: 1     Monitor Number    Selects the monitor indox for the second window. Valid values: 0...[Monitors connected minus 1]
-set arg1=%1
-start MVTT.exe %arg1% 1
